a2795c763ae87e56d76cefbd02ee60e8671a5274
Sellassie5 <enochskmortu@gmail.com>
2026-05-30 02:57:31 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials in large stylized block letters.
+# Assignment Number: 1
+#
+# Name: ENOCH SELASI DAGADU MORTU
+# STUDENT ID: 2425404190
+# Email: 2425404190@live.gctu.edu.gh
+# Grader: Carolyn
+#
+# On my honor, ENOCH SELASI DAGADU MORTU, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # Print initials in small and large letter formats using only print() and strings.
+    print()
+    print("...ESDM")
+    print()
+    print("...EEEEEEEEEEEE....SSSSSSSSSS.........DDDDDDDDD........MM........MM...")
+    print("...EE.............SS..................DD.......DD......MMM......MMM...")
+    print("...EE.............SS..................DD........DD.....MMMM....MMMM...")
+    print("...EE.............SS..................DD........DD.....MM.MM..MM.MM...")
+    print("...EEEEEEEE........SSSSSSSSSS.........DD........DD.....MM..MMMM..MM...")
+    print("...EEEEEEEE.................SS........DD........DD.....MM...MM...MM...")
+    print("...EE.......................SS........DD........DD.....MM........MM...")
+    print("...EE.......................SS........DD.......DD......MM........MM...")
+    print("...EE...........**..........SS...**...DD......DD...**...MM.......MM...**")
+    print("...EEEEEEEEEEEE.**...SSSSSSSSS...**...DDDDDDDD...**...MM.........MM...**")
+    print()
 
 
 main()
